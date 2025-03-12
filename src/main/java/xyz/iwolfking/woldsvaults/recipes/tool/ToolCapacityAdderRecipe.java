ca9267c8d6435a4a6a984af7996ebae39a3a1562
iwolfking <thegreatiwk@gmail.com>
2025-03-12 10:55:33 -0500

    Fix Resonating Reinforcement bug

--- src/main/java/xyz/iwolfking/woldsvaults/recipes/tool/ToolCapacityAdderRecipe.java
+++ src/main/java/xyz/iwolfking/woldsvaults/recipes/tool/ToolCapacityAdderRecipe.java
@@ -7,23 +7,24 @@ import iskallia.vault.item.crystal.recipe.AnvilContext;
 import iskallia.vault.item.crystal.recipe.VanillaAnvilRecipe;
 import iskallia.vault.item.tool.ToolItem;
 import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 import xyz.iwolfking.woldsvaults.config.forge.WoldsVaultsConfig;
 import xyz.iwolfking.woldsvaults.init.ModItems;
 
 public class ToolCapacityAdderRecipe extends VanillaAnvilRecipe {
 
+    private static final String REINFORCEMENT_COUNT_TAG = "woldsvaults.reinforcements_applied";
 
     @Override
     public boolean onSimpleCraft(AnvilContext context) {
 
         ItemStack primary = context.getInput()[0];
         ItemStack secondary = context.getInput()[1];
-        if (primary.getItem() instanceof ToolItem && secondary.getItem() == ModItems.RESONATING_REINFORCEMENT) {
+        if (primary.getItem() instanceof ToolItem tool && secondary.getItem() == ModItems.RESONATING_REINFORCEMENT) {
             ItemStack output = primary.copy();
             VaultGearData gear = ToolGearData.read(output);
             int toolCapacity = 0;
-            int normalMaxCapacity = 0;
             if(gear.getFirstValue(ModGearAttributes.TOOL_CAPACITY).isPresent()) {
                 toolCapacity = gear.getFirstValue(ModGearAttributes.TOOL_CAPACITY).get();
             }
@@ -31,21 +32,23 @@ public class ToolCapacityAdderRecipe extends VanillaAnvilRecipe {
                 return false;
             }
 
-            if(gear.getFirstValue(ModGearAttributes.TOOL_MATERIAL).isPresent()) {
-                normalMaxCapacity = gear.getFirstValue(ModGearAttributes.TOOL_MATERIAL).get().getCapacity();
+            CompoundTag tag = output.getOrCreateTag();
+            int reinforcementsApplied = 0;
+
+            if (tag.contains(REINFORCEMENT_COUNT_TAG)) {
+                reinforcementsApplied = tag.getInt(REINFORCEMENT_COUNT_TAG);
             }
-            else {
+
+            if (reinforcementsApplied >= 2) {
                 return false;
             }
 
-            if(toolCapacity < (normalMaxCapacity + WoldsVaultsConfig.COMMON.crystalReinforcementMaxCapacityAdded.get())) {
-                gear.createOrReplaceAttributeValue(ModGearAttributes.TOOL_CAPACITY, toolCapacity + 10);
-            }
-            else {
-                return false;
-            }
-
+            gear.createOrReplaceAttributeValue(ModGearAttributes.TOOL_CAPACITY, toolCapacity + 10);
             gear.write(output);
+
+            reinforcementsApplied++;
+            tag.putInt(REINFORCEMENT_COUNT_TAG, reinforcementsApplied);
+
             context.setOutput(output);
 
             context.onTake(context.getTake().append(() -> {
