6b1e8e9635d1b59445307a7a335a9d814488bba2
iwolfking <thegreatiwk@gmail.com>
2025-03-18 09:09:45 -0500

    Change VHAPI models to register normally

--- src/main/java/xyz/iwolfking/woldsvaults/models/AdditionalModels.java
+++ src/main/java/xyz/iwolfking/woldsvaults/models/AdditionalModels.java
@@ -5,6 +5,7 @@ import iskallia.vault.dynamodel.DynamicModelProperties;
 import iskallia.vault.dynamodel.model.item.HandHeldModel;
 import iskallia.vault.dynamodel.model.item.PlainItemModel;
 import iskallia.vault.init.ModDynamicModels;
+import xyz.iwolfking.vhapi.VHAPI;
 
 
 public class AdditionalModels {
@@ -19,12 +20,19 @@ public class AdditionalModels {
     public static final HandHeldModel HEXBLADE;
     public static final HandHeldModel HORSE_AXE;
     public static final HandHeldModel ARROGANTE;
+    public static final HandHeldModel WOLDIANCHOR;
     public static final HandHeldModel YOUNG_KITSUNE;
     public static final HandHeldModel MINERAL_GREATSWORD;
     public static final HandHeldModel GRASS_BLADE;
     public static final HandHeldModel MYTHIC_ZEKE_SWORD;
     public static final HandHeldModel SACRED_ZEKE_SWORD;
     public static final HandHeldModel PRETTY_SCISSORS;
+    public static final HandHeldModel HOTDOG_BAT;
+    public static final HandHeldModel HYPER_CRYSTAL_SWORD;
+    public static final HandHeldModel BRRYS_LUTE;
+    public static final HandHeldModel CHEESEBLADE;
+    public static final PlainItemModel CAT_WAND;
+    public static final PlainItemModel MUSTARD;
 
     static {
        LEVIATHAN_AXE = ModDynamicModels.Axes.REGISTRY.register(new HandHeldModel(VaultMod.id("gear/axe/leviathan"), "Leviathan Axe")).properties(new DynamicModelProperties());
@@ -37,12 +45,19 @@ public class AdditionalModels {
        HEXBLADE = ModDynamicModels.Swords.REGISTRY.register(new HandHeldModel(VaultMod.id("gear/sword/hexblade"), "Hexblade")).properties(new DynamicModelProperties());
        HORSE_AXE = ModDynamicModels.Axes.REGISTRY.register(new HandHeldModel(VaultMod.id("gear/axe/zombie_horse"), "Zombie-Horse Axe")).properties(new DynamicModelProperties());
        ARROGANTE = ModDynamicModels.Axes.REGISTRY.register(new HandHeldModel(VaultMod.id("gear/axe/arrogante"), "Arrogante Axe")).properties(new DynamicModelProperties());
+       WOLDIANCHOR = ModDynamicModels.Axes.REGISTRY.register(new HandHeldModel(VHAPI.of("gear/axe/iskallianchor"), "Woldianchor")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
        YOUNG_KITSUNE = ModDynamicModels.Swords.REGISTRY.register(new HandHeldModel(VaultMod.id("gear/sword/young_kitsune"), "Young Kitsune Blade")).properties(new DynamicModelProperties());
        MINERAL_GREATSWORD = ModDynamicModels.Swords.REGISTRY.register(new HandHeldModel(VaultMod.id("gear/sword/mineral_greatsword"), "Mineral Greatsword")).properties(new DynamicModelProperties());
        GRASS_BLADE = ModDynamicModels.Swords.REGISTRY.register(new HandHeldModel(VaultMod.id("gear/sword/grass_blade"), "Leaf Sword")).properties(new DynamicModelProperties());
        MYTHIC_ZEKE_SWORD = ModDynamicModels.Swords.REGISTRY.register(new HandHeldModel(VaultMod.id("gear/sword/mythic_zeke_sword"), "Mythical Blade")).properties(new DynamicModelProperties());
        SACRED_ZEKE_SWORD = ModDynamicModels.Swords.REGISTRY.register(new HandHeldModel(VaultMod.id("gear/sword/sacred_zeke_sword"), "Sacred Blade")).properties(new DynamicModelProperties());
        PRETTY_SCISSORS = ModDynamicModels.Swords.REGISTRY.register(new HandHeldModel(VaultMod.id("gear/sword/aurora_scissors"), "Aurora Scissors")).properties(new DynamicModelProperties());
+       HOTDOG_BAT = ModDynamicModels.Swords.REGISTRY.register(new HandHeldModel(VHAPI.of("gear/sword/hotdog"), "Hotdog Bat")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
+       HYPER_CRYSTAL_SWORD = ModDynamicModels.Swords.REGISTRY.register(new HandHeldModel(VHAPI.of("gear/sword/crystal"), "Hyper Crystal Sword")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
+       BRRYS_LUTE = ModDynamicModels.Swords.REGISTRY.register(new HandHeldModel(VHAPI.of("gear/sword/lute"), "Brry's Lute")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
+       CHEESEBLADE = ModDynamicModels.Swords.REGISTRY.register(new HandHeldModel(VHAPI.of("gear/sword/cheese"), "Cheeseblade")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
+       CAT_WAND = ModDynamicModels.Wands.REGISTRY.register(new PlainItemModel(VHAPI.of("gear/wand/cat"), "Cat Wand")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
+       MUSTARD = ModDynamicModels.Wands.REGISTRY.register(new PlainItemModel(VHAPI.of("gear/wand/mustard"), "Mustard Bottle")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
     }
 
 }
