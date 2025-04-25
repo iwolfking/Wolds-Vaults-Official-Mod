c9d22d434137efb04b531ccb4bce81accda0dc3c
a1qs <ankiller2007.09.20@gmail.com>
2025-04-25 18:10:15 +0200

    feat: all tooltips now get corrupted inside Corrupted Vaults

---
+++ src/main/java/xyz/iwolfking/woldsvaults/events/client/CorruptionTextEvent.java
@@ -0,0 +1,32 @@
+package xyz.iwolfking.woldsvaults.events.client;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import xyz.iwolfking.woldsvaults.util.ComponentUtils;
+import xyz.iwolfking.woldsvaults.util.VaultUtil;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, value = {Dist.CLIENT})
+public class CorruptionTextEvent {
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void corruptTooltips(ItemTooltipEvent event) {
+        if(!VaultUtil.isVaultCorrupted) return;
+
+        List<Component> toolTip = event.getToolTip();
+
+        for (int i = 0; i < toolTip.size(); i++) {
+            if(toolTip.get(i) instanceof MutableComponent cmp) {
+                toolTip.set(i, ComponentUtils.corruptComponent(cmp));
+            }
+        }
+    }
+
+}
