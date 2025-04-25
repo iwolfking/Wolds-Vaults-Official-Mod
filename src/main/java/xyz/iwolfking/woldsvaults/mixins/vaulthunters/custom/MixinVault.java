5598860546529cf917de0ecd59f25d59d6d6f029
a1qs <ankiller2007.09.20@gmail.com>
2025-04-25 18:13:58 +0200

    feat: ensure that shaders & other corrupted events stop if the client is released

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinVault.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinVault.java
@@ -1,10 +1,26 @@
 package xyz.iwolfking.woldsvaults.mixins.vaulthunters.custom;
 
 import iskallia.vault.core.vault.Vault;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.iwolfking.woldsvaults.util.VaultUtil;
 
 @Mixin(value = Vault.class, remap = false)
 public class MixinVault {
+    @Inject(method = "releaseClient", at = @At("HEAD"))
+    private void releaseCorrupted(CallbackInfo ci) {
+        if (VaultUtil.isVaultCorrupted) {
+            Minecraft mc = Minecraft.getInstance();
+            GameRenderer render = mc.gameRenderer;
 
+            if (render.currentEffect() != null) { // Check if any shader is active
+                render.shutdownEffect(); // Unload the current shader
+            }
+            VaultUtil.isVaultCorrupted = false;
+        }
+    }
 }
