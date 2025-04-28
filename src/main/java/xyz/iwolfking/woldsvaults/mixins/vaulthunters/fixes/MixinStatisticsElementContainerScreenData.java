548ad672d0830ad6b1dd370bdd36978911308cf9
iwolfking <thegreatiwk@gmail.com>
2025-04-28 11:57:04 -0500

    The player stats screen now displays a lot more stats

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/fixes/MixinStatisticsElementContainerScreenData.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/fixes/MixinStatisticsElementContainerScreenData.java
@@ -23,11 +23,23 @@ public class MixinStatisticsElementContainerScreenData {
 
     @Inject(method = "getStatListPlayer", at = @At("TAIL"))
     private void addNewDamageStats(CallbackInfoReturnable<List<StatLabelElementBuilder<?>>> cir, @Local List<StatLabelElementBuilder<?>> result) {
-        result.add( GearAttributeStatLabel.ofFloat(this.player, ModGearAttributes.DAMAGE_HORDE));
-        result.add( GearAttributeStatLabel.ofFloat(this.player, ModGearAttributes.DAMAGE_TANK));
-        result.add( GearAttributeStatLabel.ofFloat(this.player, ModGearAttributes.DAMAGE_ASSASSIN));
-        result.add( GearAttributeStatLabel.ofFloat(this.player, ModGearAttributes.DAMAGE_DWELLER));
-        result.add( GearAttributeStatLabel.ofFloat(this.player, ModGearAttributes.DAMAGE_CHAMPION));
-        //result.add( GearAttributeStatLabel.ofFloat(this.player, ModGearAttributes.DAMAGE_DUNGEON));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, ModGearAttributes.IDONA_AFFINITY));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, ModGearAttributes.TENOS_AFFINITY));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, ModGearAttributes.VELARA_AFFINITY));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, ModGearAttributes.WENDARR_AFFINITY));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, ModGearAttributes.HIT_HEARTS));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, ModGearAttributes.FRUIT_EFFECTIVENESS));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, xyz.iwolfking.woldsvaults.init.ModGearAttributes.DODGE_PERCENT));
+        result.add( GearAttributeStatLabel.ofInteger(this.player, xyz.iwolfking.woldsvaults.init.ModGearAttributes.SOUL_LEECH_FLAT));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, xyz.iwolfking.woldsvaults.init.ModGearAttributes.AP_SCALING_DAMAGE));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, xyz.iwolfking.woldsvaults.init.ModGearAttributes.CHAINING_DAMAGE));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, xyz.iwolfking.woldsvaults.init.ModGearAttributes.THORNS_SCALING_DAMAGE));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, xyz.iwolfking.woldsvaults.init.ModGearAttributes.CHANNELING_CHANCE));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, xyz.iwolfking.woldsvaults.init.ModGearAttributes.DISMANTLE_CHANCE));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, xyz.iwolfking.woldsvaults.init.ModGearAttributes.ECHOING_CHANCE));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, xyz.iwolfking.woldsvaults.init.ModGearAttributes.ECHOING_DAMAGE));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, xyz.iwolfking.woldsvaults.init.ModGearAttributes.HEXING_CHANCE));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, xyz.iwolfking.woldsvaults.init.ModGearAttributes.EXECUTION_DAMAGE));
+        result.add( GearAttributeStatLabel.ofFloat(this.player, xyz.iwolfking.woldsvaults.init.ModGearAttributes.REAVING_DAMAGE));
     }
 }
