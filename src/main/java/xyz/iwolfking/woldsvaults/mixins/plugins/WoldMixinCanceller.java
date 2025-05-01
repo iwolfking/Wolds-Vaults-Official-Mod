818d4e0510a0e26eb081b173c8820abddb929a08
iwolfking <thegreatiwk@gmail.com>
2025-05-01 11:22:55 -0500

    Don't cancel QOL hunter mixins

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/plugins/WoldMixinCanceller.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/plugins/WoldMixinCanceller.java
@@ -22,18 +22,6 @@ public class WoldMixinCanceller implements MixinCanceller {
             else if(s.equals("io.iridium.qolhunters.mixin.paradox.MixinGateLockRenderer")) {
                 return true;
             }
-            else if(s.equals("io.iridium.qolhunters.mixin.transmogtable.MixinDiscoveredModelSelectorModel")) {
-                return true;
-            }
-            else if(s.equals("io.iridium.qolhunters.mixin.transmogtable.MixinDiscoveredModelSelectElement")) {
-                return true;
-            }
-            else if(s.equals("io.iridium.qolhunters.mixin.ascenscionforge.MixinAscensionForgeSelectElement")) {
-                return true;
-            }
-            else if(s.equals("io.iridium.qolhunters.mixin.ascenscionforge.MixinDiscoveredModelSelectorModel")) {
-                return true;
-            }
         }
 
         if(LoadingModList.get().getModFileById("unobtainium") != null) {
