20ec475f93d53c1f7cfe450c2788c5077d06ade8
iwolfking <thegreatiwk@gmail.com>
2025-05-15 20:23:09 -0500

    Add special corruption interaction for maps

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinCorruptGearModification.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinCorruptGearModification.java
@@ -1,17 +1,24 @@
 package xyz.iwolfking.woldsvaults.mixins.vaulthunters.custom;
 
+import iskallia.vault.VaultMod;
+import iskallia.vault.config.UniqueGearConfig;
+import iskallia.vault.core.random.JavaRandom;
 import iskallia.vault.gear.VaultGearModifierHelper;
 import iskallia.vault.gear.attribute.VaultGearAttribute;
 import iskallia.vault.gear.attribute.VaultGearAttributeInstance;
 import iskallia.vault.gear.attribute.VaultGearModifier;
 import iskallia.vault.gear.data.VaultGearData;
+import iskallia.vault.gear.item.VaultGearItem;
 import iskallia.vault.gear.modification.GearModification;
+import iskallia.vault.init.ModConfigs;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import xyz.iwolfking.woldsvaults.WoldsVaults;
 import xyz.iwolfking.woldsvaults.init.ModGearAttributes;
+import xyz.iwolfking.woldsvaults.items.gear.VaultMapItem;
 
 import java.util.List;
 import java.util.Random;
@@ -28,6 +35,12 @@ public class MixinCorruptGearModification {
         if (!data.isModifiable()) {
             return GearModification.Result.errorUnmodifiable();
         } else {
+            if(stack.getItem() instanceof VaultMapItem) {
+                if(JavaRandom.ofNanoTime().nextFloat() <= 0.1F) {
+                    data.createOrReplaceAttributeValue(ModGearAttributes.OBJECTIVE, "corrupted");
+                    data.createOrReplaceAttributeValue(ModGearAttributes.THEME_POOL, VaultMod.sId("corrupted"));
+                }
+            }
             if(data.hasAttribute(ModGearAttributes.DIVINE)) {
                 Random random = new Random();
                 if(random.nextBoolean()) {
