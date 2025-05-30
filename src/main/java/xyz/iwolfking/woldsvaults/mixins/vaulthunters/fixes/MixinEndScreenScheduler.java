8548bb1d3d67d6b61b9c28fbb638ec99e0e99718
iwolfking <thegreatiwk@gmail.com>
2025-05-30 09:17:32 -0500

    Add possible bandaid for end screen crashes

---
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/fixes/MixinEndScreenScheduler.java
@@ -0,0 +1,43 @@
+package xyz.iwolfking.woldsvaults.mixins.vaulthunters.fixes;
+
+import iskallia.vault.client.gui.screen.summary.VaultEndScreen;
+import iskallia.vault.core.event.client.ClientTickEvent;
+import iskallia.vault.util.scheduler.EndScreenScheduler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.TextComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = EndScreenScheduler.class, remap = false)
+public abstract class MixinEndScreenScheduler {
+    @Shadow
+    public static EndScreenScheduler getInstance() {
+        return null;
+    }
+
+    /**
+     * @author iwolfking
+     * @reason Test
+     */
+    @Redirect(method = "onClientTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;setScreen(Lnet/minecraft/client/gui/screens/Screen;)V"), remap = true)
+    private static void onSetScreen(Minecraft instance, Screen pGuiScreen) {
+        try {
+            if(getInstance() != null) {
+                if(Minecraft.getInstance().player != null) {
+                    Minecraft.getInstance().setScreen(new VaultEndScreen(getInstance().snapshot, new TextComponent("Vault Exit"), Minecraft.getInstance().player.getUUID()));
+                    getInstance().snapshot = null;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("There was an issue displaying the end screen for the vault.");
+            e.printStackTrace();
+            if(getInstance() != null) {
+                getInstance().snapshot = null;
+            }
+        }
+    }
+}
