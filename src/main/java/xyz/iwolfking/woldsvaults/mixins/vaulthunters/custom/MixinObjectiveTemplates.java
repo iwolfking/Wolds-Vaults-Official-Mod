d3f24d972a02db878dd4af2021f1a88f8a0a0b23
iwolfking <thegreatiwk@gmail.com>
2025-06-02 10:14:53 -0500

    Add remap to mixin

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinObjectiveTemplates.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinObjectiveTemplates.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.iwolfking.woldsvaults.init.ModObjectiveTemplates;
 import xyz.iwolfking.woldsvaults.objectives.CorruptedObjective;
 
-@Mixin(ObjectiveTemplateEvent.class)
+@Mixin(value = ObjectiveTemplateEvent.class, remap = false)
 public class MixinObjectiveTemplates {
     @Inject(method = "getTemplateForObjective", at = @At("HEAD"), cancellable = true)
     private void getWoldsObjectiveTemplates(Objective objective, CallbackInfoReturnable<Template> cir) {
