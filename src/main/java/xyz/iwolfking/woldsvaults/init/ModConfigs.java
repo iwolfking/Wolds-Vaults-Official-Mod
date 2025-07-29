22b7405580ec27a1adbfdbb3c11f2a264a22f6c1
iwolfking <thegreatiwk@gmail.com>
2025-07-29 10:33:24 -0500

    Add ResearchExclusionConfig which can be used to exclude certain blocks/items from being restricted

--- src/main/java/xyz/iwolfking/woldsvaults/init/ModConfigs.java
+++ src/main/java/xyz/iwolfking/woldsvaults/init/ModConfigs.java
@@ -51,6 +51,7 @@ public class ModConfigs {
     public static VaultGearRarityColorConfig VAULT_GEAR_RARITY_COLOR_CONFIG = new VaultGearRarityColorConfig();
     public static TrinketPouchConfig TRINKET_POUCH = new TrinketPouchConfig();
     public static RecipeUnlocksConfig RECIPE_UNLOCKS = new RecipeUnlocksConfig();
+    public static ResearchExclusionConfig RESEARCH_EXCLUSIONS = new ResearchExclusionConfig();
 
     public static void preregister() {
         VAULT_GEAR_RARITY_COLOR_CONFIG = new VaultGearRarityColorConfig().readConfig();
@@ -90,5 +91,6 @@ public class ModConfigs {
         TRINKET_POUCH = new TrinketPouchConfig().readConfig();
         RECIPE_UNLOCKS = new RecipeUnlocksConfig().readConfig();
         WEAVING_RECIPES_CONFIG = new WeavingRecipesConfig().readConfig();
+        RESEARCH_EXCLUSIONS = new ResearchExclusionConfig().readConfig();
     }
 }
