a13659c6a0654f7c12e869a03643dc234bd4f7cb
iwolfking <thegreatiwk@gmail.com>
2025-07-30 15:22:31 -0500

    Add new gamerules for vault dolls

--- src/main/java/xyz/iwolfking/woldsvaults/init/ModGameRules.java
+++ src/main/java/xyz/iwolfking/woldsvaults/init/ModGameRules.java
@@ -1,21 +1,36 @@
 package xyz.iwolfking.woldsvaults.init;
 
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.event.OnDatapackSyncEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import xyz.iwolfking.woldsvaults.WoldsVaults;
+import xyz.iwolfking.woldsvaults.api.helper.GameruleHelper;
 import xyz.iwolfking.woldsvaults.mixins.GameRulesBooleanValueAccessor;
+import xyz.iwolfking.woldsvaults.network.message.ClientboundSyncGamerulesMessage;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
 
+@Mod.EventBusSubscriber(modid = WoldsVaults.MOD_ID)
 public class ModGameRules {
 
     public static GameRules.Key<GameRules.BooleanValue> ALLOW_FLIGHT_IN_VAULTS;
     public static GameRules.Key<GameRules.BooleanValue> NORMALIZED_ENABLED;
     public static GameRules.Key<GameRules.BooleanValue> ENABLE_OLD_AFFINITY_HANDLING;
+    public static GameRules.Key<GameRules.BooleanValue> ENABLE_PLACING_VAULT_DOLLS;
+    public static GameRules.Key<GameRules.BooleanValue> ENABLE_VAULT_DOLLS;
 
     public static void initialize() {
         ALLOW_FLIGHT_IN_VAULTS = GameRules.register("enableFlightInVaults", GameRules.Category.PLAYER, booleanRule(false));
         NORMALIZED_ENABLED = GameRules.register("enableDifficultyLockModifiers", GameRules.Category.PLAYER, booleanRule(true));
         ENABLE_OLD_AFFINITY_HANDLING = GameRules.register("enableLegacyGodAffinityHandling", GameRules.Category.PLAYER, booleanRule(false));
+        ENABLE_PLACING_VAULT_DOLLS = GameRules.register("enablePlacingVaultDolls", GameRules.Category.PLAYER, booleanRule(false));
+        ENABLE_VAULT_DOLLS = GameRules.register("enableVaultDolls", GameRules.Category.PLAYER, booleanRule(true));
     }
 
 
@@ -23,8 +38,15 @@ public class ModGameRules {
         return GameRulesBooleanValueAccessor.create(defaultValue, ((minecraftServer, booleanValue) -> {}));
     }
 
-    public static boolean isEnabled(GameRules.Key<GameRules.BooleanValue> gamerule, Level level) {
-        return level.getGameRules().getBoolean(gamerule);
+    @SubscribeEvent
+    public static void syncGameRules(OnDatapackSyncEvent event) {
+        ServerPlayer player = event.getPlayer();
+        if(player == null) {
+            GameruleHelper.syncGameRules(event.getPlayerList().getPlayers());
+        }
+        else {
+            GameruleHelper.syncGameRules(player);
+        }
     }
    
 }
