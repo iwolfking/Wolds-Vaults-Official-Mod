8a2371ebb503033e8d4d491106866bb78bb8ae82
iwolfking <thegreatiwk@gmail.com>
2025-07-30 16:56:57 -0500

    Actually fix XP issues (and cleanup mixin)

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinPlayerVaultStats.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinPlayerVaultStats.java
@@ -1,28 +1,26 @@
 package xyz.iwolfking.woldsvaults.mixins.vaulthunters.custom;
 
 
+import com.llamalad7.mixinextras.sugar.Local;
 import dev.ftb.mods.ftbquests.quest.ServerQuestFile;
 import dev.ftb.mods.ftbquests.quest.TeamData;
-import iskallia.vault.core.event.CommonEvents;
-import iskallia.vault.event.event.VaultLevelUpEvent;
-import iskallia.vault.init.ModConfigs;
+import iskallia.vault.config.VaultLevelsConfig;
 import iskallia.vault.skill.PlayerVaultStats;
 import iskallia.vault.skill.base.Skill;
 import iskallia.vault.skill.expertise.type.ExperiencedExpertise;
 import iskallia.vault.skill.tree.ExpertiseTree;
-import iskallia.vault.util.NetcodeUtils;
 import iskallia.vault.world.data.PlayerExpertisesData;
-import iskallia.vault.world.data.PlayerGreedData;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.iwolfking.woldsvaults.integration.ftbquests.tasks.VaultLevelTask;
-import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.List;
@@ -34,91 +32,47 @@ public abstract class MixinPlayerVaultStats {
 
     @Shadow private int exp;
     @Shadow private int vaultLevel;
-
-    @Shadow public abstract int getExpNeededToNextLevel();
-
-    @Shadow private int unspentSkillPoints;
-    @Shadow private int unspentExpertisePoints;
     @Shadow @Final private UUID uuid;
 
-    @Shadow protected abstract void fancyLevelUpEffects(ServerPlayer player);
-
-    @Shadow public abstract void sync(MinecraftServer server);
-
-    @Shadow private int unspentPrestigePoints;
-
-    /**
-     * @author iwolfking
-     * @reason
-     */
-    @Overwrite
-    public void addVaultExp(MinecraftServer server, int exp) {
-        this.exp = Math.max(this.exp, 0);
-        this.exp += (int) ((float) exp * ModConfigs.LEVELS_META.getExpMultiplier());
+    @Redirect(method = "addVaultExp", at = @At(value = "INVOKE", target = "Liskallia/vault/config/VaultLevelsConfig;getExpMultiplier()F"))
+    private float addExperiencedExpertiseMultiplier(VaultLevelsConfig instance, @Local MinecraftServer server) {
         ServerPlayer player = server.getPlayerList().getPlayer(this.uuid);
-        if (player == null) {
-            return;
+        if(player == null) {
+            return instance.getExpMultiplier();
         }
+
         ExpertiseTree expertises = PlayerExpertisesData.get(player.getLevel()).getExpertises(player);
         float increase = 0.0F;
         for (ExperiencedExpertise expertise1 : expertises.getAll(ExperiencedExpertise.class, Skill::isUnlocked)) {
             increase += expertise1.getIncreasedExpPercentage();
         }
-        this.exp = (int) (this.exp + (this.exp * increase));
-        int maxLevel = ModConfigs.LEVELS_META.getMaxLevel();
-        if (this.getVaultLevel() >= 100 || this.getVaultLevel() < maxLevel) {
-            int initialLevel = this.vaultLevel;
-            if (initialLevel >= 100 && !PlayerGreedData.get(server).get(this.uuid).hasCompletedHerald() && FMLEnvironment.production) {
-                this.exp = 0;
-                this.vaultLevel = 100;
-            } else {
-                int neededExp;
-                for (; this.exp >= (neededExp = this.getExpNeededToNextLevel()); this.exp -= neededExp) {
-                    ++this.vaultLevel;
-                    if (this.vaultLevel > 100) {
-                        ++this.unspentPrestigePoints;
-                    } else {
-                        ++this.unspentSkillPoints;
-                        if (this.vaultLevel % 5 == 0) {
-                            ++this.unspentExpertisePoints;
-                        }
-                    }
-                }
 
-                if (this.vaultLevel > initialLevel) {
-                    NetcodeUtils.runIfPresent(server, this.uuid, this::fancyLevelUpEffects);
-                    player.refreshTabListName();
-
-                    if (this.vaultLevel > initialLevel) {
-                        NetcodeUtils.runIfPresent(server, this.uuid, this::fancyLevelUpEffects);
-                        player.refreshTabListName();
-                        woldsVaults$vaultLevelTaskProgress(player, exp, initialLevel, this.vaultLevel);
-                        MinecraftForge.EVENT_BUS.post(new VaultLevelUpEvent(player, exp, initialLevel, this.vaultLevel));
-                        CommonEvents.VAULT_LEVEL_UP.invoke(player, exp, initialLevel, this.vaultLevel);
-                    }
-
-                    this.sync(server);
-                }
-            }
-        }
+        return instance.getExpMultiplier() + increase;
     }
 
-    private List<VaultLevelTask> levelTasks = null;
+    @Inject(method = "addVaultExp", at = @At(value = "INVOKE", target = "Liskallia/vault/core/event/common/VaultLevelUpEvent;invoke(Lnet/minecraft/server/level/ServerPlayer;III)Liskallia/vault/core/event/common/VaultLevelUpEvent$Data;"))
+    private void progressFTBQuestsTasks(MinecraftServer server, int exp, CallbackInfo ci, @Local ServerPlayer player) {
+        woldsVaults$vaultLevelTaskProgress(player, this.vaultLevel);
+    }
 
 
     @Unique
-    public void woldsVaults$vaultLevelTaskProgress(Player player, int exp, int initialLevel, int newLevel) {
-        if (levelTasks == null) {
-            levelTasks = ServerQuestFile.INSTANCE.collect(VaultLevelTask.class);
+    private List<VaultLevelTask> woldsVaults$levelTasks = null;
+
+
+    @Unique
+    public void woldsVaults$vaultLevelTaskProgress(Player player, int newLevel) {
+        if (woldsVaults$levelTasks == null) {
+            woldsVaults$levelTasks = ServerQuestFile.INSTANCE.collect(VaultLevelTask.class);
         }
 
-        if (levelTasks.isEmpty()) {
+        if (woldsVaults$levelTasks.isEmpty()) {
             return;
         }
 
         TeamData data = ServerQuestFile.INSTANCE.getData(player);
 
-        for (VaultLevelTask task : levelTasks) {
+        for (VaultLevelTask task : woldsVaults$levelTasks) {
             if (data.getProgress(task) < task.getMaxProgress() && data.canStartTasks(task.quest)) {
                 data.setProgress(task, newLevel);
             }
--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/fixes/MixinVaultXPFoodItem.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/fixes/MixinVaultXPFoodItem.java
@@ -1,24 +1,10 @@
 package xyz.iwolfking.woldsvaults.mixins.vaulthunters.fixes;
 
-import com.llamalad7.mixinextras.sugar.Local;
-import iskallia.vault.event.ActiveFlags;
+
 import iskallia.vault.item.VaultXPFoodItem;
-import iskallia.vault.world.data.PlayerVaultStatsData;
-import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.Slice;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = VaultXPFoodItem.Flat.class, remap = false)
 public class MixinVaultXPFoodItem {
-    @Inject(method = "grantExp", at = @At(value = "INVOKE", target = "Liskallia/vault/event/ActiveFlags;runIfNotSet(Ljava/lang/Runnable;)V"), slice = @Slice(from = @At(value = "INVOKE", target = "Liskallia/vault/util/MathUtilities;getRandomInt(II)I"), to = @At(value = "INVOKE", target = "Liskallia/vault/event/ActiveFlags;runIfNotSet(Ljava/lang/Runnable;)V")), cancellable = true)
-    private void replaceRunIfNotSet(ServerPlayer sPlayer, int count, CallbackInfo ci, @Local PlayerVaultStatsData data, @Local(ordinal = 1) int exp) {
-        ActiveFlags.IS_BURGER_XP.push();
-        data.addVaultExp(sPlayer, exp);
-        ActiveFlags.IS_BURGER_XP.pop();
-        ci.cancel();
-    }
+
 }
