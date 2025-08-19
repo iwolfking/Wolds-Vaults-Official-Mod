79b2e7403447e2ac9410a903665fbb03cc6dc35f
iwolfking <thegreatiwk@gmail.com>
2025-08-19 09:30:17 -0500

    Add unfinished companion temporal rerolling item

---
+++ src/main/java/xyz/iwolfking/woldsvaults/items/CompanionRerollingItem.java
@@ -0,0 +1,63 @@
+package xyz.iwolfking.woldsvaults.items;
+
+import com.github.alexthe666.alexsmobs.misc.AMSoundRegistry;
+import iskallia.vault.block.entity.CompanionHomeTileEntity;
+import iskallia.vault.block.entity.VaultAltarTileEntity;
+import iskallia.vault.core.vault.modifier.registry.VaultModifierRegistry;
+import iskallia.vault.core.vault.modifier.spi.VaultModifier;
+import iskallia.vault.init.ModSounds;
+import iskallia.vault.item.BasicItem;
+import iskallia.vault.item.CompanionItem;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+
+public class CompanionRerollingItem extends BasicItem {
+    public CompanionRerollingItem(ResourceLocation id) {
+        super(id);
+    }
+
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        Level level = context.getLevel();
+        ItemStack stackInHand = context.getItemInHand();
+        if(level.isClientSide) {
+            return InteractionResult.PASS;
+        }
+
+        if(level.getBlockEntity(context.getClickedPos()) instanceof CompanionHomeTileEntity home && stackInHand.getItem() instanceof CompanionRerollingItem) {
+            if(!home.getCompanion().isEmpty()) {
+                ItemStack companion = home.getCompanion();
+                if(stackInHand.hasTag()) {
+                    VaultModifier<?> modifier;
+                    if(stackInHand.getOrCreateTag().contains("modifier")) {
+                        String modifierId = stackInHand.getOrCreateTag().getString("modifier");
+                        modifier = VaultModifierRegistry.get(new ResourceLocation(modifierId));
+                    }
+                    else {
+                        modifier = CompanionItem.getRandomTemporalModifier();
+                    }
+
+                    CompanionItem.setTemporalModifier(companion, modifier.getId());
+                    stackInHand.shrink(1);
+                    if(context.getPlayer() != null) {
+                        context.getPlayer().displayClientMessage(new TextComponent("Your Companion now has the ").append(modifier.getNameComponent()).append(" modifier!"), true);
+                    }
+
+                    return InteractionResult.SUCCESS;
+                }
+            }
+        }
+
+        return InteractionResult.PASS;
+    }
+
+}
