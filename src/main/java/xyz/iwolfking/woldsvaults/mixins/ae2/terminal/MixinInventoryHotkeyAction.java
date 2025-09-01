4bd5523107fa937e3e6ba5abe8a2797650610188
radimous <jurda92@gmail.com>
2025-09-01 17:49:17 +0200

    maybe the original was better tbh :(

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/ae2/terminal/MixinInventoryHotkeyAction.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/ae2/terminal/MixinInventoryHotkeyAction.java
@@ -1,11 +1,11 @@
 package xyz.iwolfking.woldsvaults.mixins.ae2.terminal;
 
-import appeng.core.definitions.AEItems;
 import appeng.hotkeys.InventoryHotkeyAction;
 import me.fallenbreath.conditionalmixin.api.annotation.Condition;
 import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.items.IItemHandlerModifiable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,7 +13,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import top.theillusivec4.curios.api.CuriosApi;
-import top.theillusivec4.curios.api.SlotResult;
 
 import java.util.function.Predicate;
 @Restriction(
@@ -29,13 +28,18 @@ public abstract class MixinInventoryHotkeyAction {
 
     @Inject(method = "run", at = @At("HEAD"), cancellable = true)
     private void runWirelessTerminalCurio(Player player, CallbackInfoReturnable<Boolean> cir) {
-        SlotResult terminalSlot = CuriosApi.getCuriosHelper().findFirstCurio(player, AEItems.WIRELESS_CRAFTING_TERMINAL.asItem()).orElse(null);
-        if (terminalSlot == null) {
-            terminalSlot = CuriosApi.getCuriosHelper().findFirstCurio(player, AEItems.WIRELESS_TERMINAL.asItem()).orElse(null);
+
+        IItemHandlerModifiable handler = CuriosApi.getCuriosHelper().getEquippedCurios(player).resolve().orElse(null);
+        if (handler == null) {
+            return;
         }
 
-        if (terminalSlot != null && this.locatable.test(terminalSlot.stack()) && opener.open(player, -1)) {
-            cir.setReturnValue(true);
+        for (int i = 0; i < handler.getSlots(); i++) {
+            ItemStack curioStack = handler.getStackInSlot(i);
+            if (this.locatable.test(curioStack) && opener.open(player, -1)) {
+                cir.setReturnValue(true);
+                return;
+            }
         }
     }
 }
