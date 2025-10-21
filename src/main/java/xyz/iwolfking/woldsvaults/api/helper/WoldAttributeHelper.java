e7cdaef0705278d874d8bc0ce286dc221f1a22f9
iwolfking <thegreatiwk@gmail.com>
2025-10-21 10:56:17 -0500

    Chaos Grenade procs Effect Clouds on explosion

--- src/main/java/xyz/iwolfking/woldsvaults/api/helper/WoldAttributeHelper.java
+++ src/main/java/xyz/iwolfking/woldsvaults/api/helper/WoldAttributeHelper.java
@@ -8,6 +8,9 @@ import iskallia.vault.snapshot.AttributeSnapshotHelper;
 import iskallia.vault.util.calc.PlayerStat;
 import iskallia.vault.util.calc.ThornsHelper;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.entity.living.LivingEvent;
+
+import java.util.function.BiConsumer;
 
 public class WoldAttributeHelper {
     public static float getAdditionalAbilityPower(LivingEntity entity) {
@@ -17,5 +20,13 @@ public class WoldAttributeHelper {
         additionalMultiplier += (Float)snapshot.getAttributeValue(ModGearAttributes.ABILITY_POWER, VaultGearAttributeTypeMerger.floatSum());
         apMulti += snapshot.getAttributeValue(ModGearAttributes.ABILITY_POWER_PERCENT, VaultGearAttributeTypeMerger.floatSum());
         return additionalMultiplier * apMulti;
+    }
+
+    public static void withSnapshot(LivingEntity entity, boolean serverOnly, BiConsumer<LivingEntity, AttributeSnapshot> fn) {
+        if (AttributeSnapshotHelper.canHaveSnapshot(entity)) {
+            if (!serverOnly || !entity.getCommandSenderWorld().isClientSide()) {
+                fn.accept(entity, AttributeSnapshotHelper.getInstance().getSnapshot(entity));
+            }
+        }
     }
 }
---
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinVaultGrenade.java
@@ -0,0 +1,67 @@
+package xyz.iwolfking.woldsvaults.mixins.vaulthunters.custom;
+
+import iskallia.vault.entity.entity.EffectCloudEntity;
+import iskallia.vault.entity.entity.VaultGrenade;
+import iskallia.vault.gear.attribute.type.VaultGearAttributeTypeMerger;
+import iskallia.vault.init.ModGearAttributes;
+import iskallia.vault.util.calc.AreaOfEffectHelper;
+import iskallia.vault.util.calc.EffectDurationHelper;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.iwolfking.woldsvaults.api.helper.WoldAttributeHelper;
+
+@Mixin(value = VaultGrenade.class, remap = false)
+public abstract class MixinVaultGrenade extends ThrowableItemProjectile {
+    public MixinVaultGrenade(EntityType<? extends ThrowableItemProjectile> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+
+    @Inject(method = "explode", at = @At(value = "INVOKE", target = "Liskallia/vault/entity/entity/VaultGrenade;createSafeExplosion(Lnet/minecraft/server/level/ServerPlayer;DDDF)V", ordinal = 0))
+    private void explodeEffectClouds(CallbackInfo ci) {
+        if(this.getOwner() instanceof ServerPlayer player) {
+            WoldAttributeHelper.withSnapshot(
+                    player,
+                    true,
+                    (playerEntity, snapshot) -> snapshot.getAttributeValue(ModGearAttributes.EFFECT_CLOUD_WHEN_HIT, VaultGearAttributeTypeMerger.asList())
+                            .forEach(cloud -> {
+                                MobEffect effect = cloud.getPrimaryEffect();
+                                if (effect != null) {
+                                        EffectCloudEntity cloudEntity = new EffectCloudEntity(playerEntity.getLevel(), this.getX(), this.getY(), this.getZ());
+                                        cloud.apply(cloudEntity);
+                                        cloudEntity.setRadius(AreaOfEffectHelper.adjustAreaOfEffect(playerEntity, null, cloudEntity.getRadius()));
+                                        cloudEntity.setOwner(playerEntity);
+                                        cloudEntity.setDuration(EffectDurationHelper.adjustEffectDurationFloor(playerEntity, cloudEntity.getDuration()));
+                                        playerEntity.getLevel().addFreshEntity(cloudEntity);
+                                }
+                            })
+            );
+
+            WoldAttributeHelper.withSnapshot(
+                    player,
+                    true,
+                    (playerEntity, snapshot) -> snapshot.getAttributeValue(ModGearAttributes.EFFECT_CLOUD, VaultGearAttributeTypeMerger.asList())
+                            .forEach(cloud -> {
+                                MobEffect effect = cloud.getPrimaryEffect();
+                                if (effect != null) {
+                                    EffectCloudEntity cloudEntity = new EffectCloudEntity(playerEntity.getLevel(), this.getX(), this.getY(), this.getZ());
+                                    cloud.apply(cloudEntity);
+                                    cloudEntity.setRadius(AreaOfEffectHelper.adjustAreaOfEffect(playerEntity, null, cloudEntity.getRadius()));
+                                    cloudEntity.setOwner(playerEntity);
+                                    cloudEntity.setDuration(EffectDurationHelper.adjustEffectDurationFloor(playerEntity, cloudEntity.getDuration()));
+                                    playerEntity.getLevel().addFreshEntity(cloudEntity);
+                                }
+                            })
+            );
+        }
+    }
+
+
+}
