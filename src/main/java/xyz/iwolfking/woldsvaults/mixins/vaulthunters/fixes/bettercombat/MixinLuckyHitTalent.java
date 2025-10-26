a70b7bc51f3d33707874df323e120dc16a7507ae
iwolfking <thegreatiwk@gmail.com>
2025-10-26 08:41:54 -0500

    Fix lucky hit not proccing without fully resetting attack with Better Combat

---
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/fixes/bettercombat/MixinLuckyHitTalent.java
@@ -0,0 +1,16 @@
+package xyz.iwolfking.woldsvaults.mixins.vaulthunters.fixes.bettercombat;
+
+import iskallia.vault.skill.talent.type.luckyhit.LuckyHitTalent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(value = LuckyHitTalent.class, remap = false)
+public class MixinLuckyHitTalent {
+    @ModifyConstant(method = "doLuckyHit", constant = @Constant(floatValue = 1.0F, ordinal = 0))
+    private static float adjustAttackScaleRequirementLuckyHit(float constant) {
+        return 0.5F;
+    }
+}
