307e059b4ca13477879818840a672990c1bd6146
iwolfking <thegreatiwk@gmail.com>
2025-11-03 10:28:07 -0600

    Further bump up treasure hunter refresh

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/fixes/MixinTreasureHunterEffect.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/fixes/MixinTreasureHunterEffect.java
@@ -9,6 +9,6 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 public class MixinTreasureHunterEffect {
     @ModifyConstant(method = "updateIndicatorEffect", constant = @Constant(intValue = 40))
     private static int updateEffectEarlier(int constant) {
-        return 100;
+        return 180;
     }
 }
