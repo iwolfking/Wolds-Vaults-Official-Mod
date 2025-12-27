cab2e30200daa334b04f261e5b8e7126f367bc23
iwolfking <thegreatiwk@gmail.com>
2025-12-27 11:51:33 -0600

    Fix Dungeons Mobs shields being useable

---
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/dungeons_mobs/MixinVanguardShield.java
@@ -0,0 +1,28 @@
+package xyz.iwolfking.woldsvaults.mixins.dungeons_mobs;
+
+import com.infamous.dungeons_mobs.items.shield.VanguardShieldItem;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ShieldItem;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.ToolAction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = VanguardShieldItem.class, remap = false)
+public class MixinVanguardShield extends ShieldItem {
+
+    public MixinVanguardShield(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
+        ItemStack itemstack = pPlayer.getItemInHand(pHand);
+        return InteractionResultHolder.pass(itemstack);
+    }
+}
