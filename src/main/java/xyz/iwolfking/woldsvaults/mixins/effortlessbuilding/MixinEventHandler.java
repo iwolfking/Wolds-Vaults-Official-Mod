de58707131d65bef8fb88da5a657b40881e7794b
iwolfking <thegreatiwk@gmail.com>
2025-12-28 11:20:39 -0600

    Make mixin conditional

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/effortlessbuilding/MixinEventHandler.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/effortlessbuilding/MixinEventHandler.java
@@ -1,31 +1,19 @@
 package xyz.iwolfking.woldsvaults.mixins.effortlessbuilding;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import iskallia.vault.block.base.InventoryRetainerBlock;
-import iskallia.vault.block.entity.base.InventoryRetainerTileEntity;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import net.minecraftforge.event.world.BlockEvent;
 import nl.requios.effortlessbuilding.EventHandler;
-import nl.requios.effortlessbuilding.buildmode.BuildModes;
-import nl.requios.effortlessbuilding.helper.SurvivalHelper;
-import nl.requios.effortlessbuilding.network.BlockPlacedMessage;
-import org.checkerframework.checker.units.qual.A;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
+@Restriction(
+        require = {
+                @Condition(type = Condition.Type.MOD, value = "effortlessbuilding")
+        }
+)
 @Mixin(value = EventHandler.class, remap = false)
 public abstract class MixinEventHandler {
 
