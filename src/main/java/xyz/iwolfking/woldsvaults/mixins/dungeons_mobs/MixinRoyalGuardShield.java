5a067adfa406059c98545df71c5d3d7e74580a3b
iwolfking <thegreatiwk@gmail.com>
2025-12-28 10:05:26 -0600

    Add some conditional mixining

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/dungeons_mobs/MixinRoyalGuardShield.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/dungeons_mobs/MixinRoyalGuardShield.java
@@ -2,6 +2,8 @@ package xyz.iwolfking.woldsvaults.mixins.dungeons_mobs;
 
 import com.infamous.dungeons_mobs.items.shield.RoyalGuardShieldItem;
 import com.infamous.dungeons_mobs.items.shield.VanguardShieldItem;
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -14,6 +16,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+@Restriction(
+        require = {
+                @Condition(type = Condition.Type.MOD, value = "dungeons_mobs")
+        }
+)
 @Mixin(value = RoyalGuardShieldItem.class, remap = false)
 public class MixinRoyalGuardShield extends ShieldItem {
     public MixinRoyalGuardShield(Properties pProperties) {
--- src/main/java/xyz/iwolfking/woldsvaults/mixins/dungeons_mobs/MixinVanguardShield.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/dungeons_mobs/MixinVanguardShield.java
@@ -1,6 +1,8 @@
 package xyz.iwolfking.woldsvaults.mixins.dungeons_mobs;
 
 import com.infamous.dungeons_mobs.items.shield.VanguardShieldItem;
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -13,6 +15,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+@Restriction(
+        require = {
+                @Condition(type = Condition.Type.MOD, value = "dungeons_mobs")
+        }
+)
 @Mixin(value = VanguardShieldItem.class, remap = false)
 public class MixinVanguardShield extends ShieldItem {
 
