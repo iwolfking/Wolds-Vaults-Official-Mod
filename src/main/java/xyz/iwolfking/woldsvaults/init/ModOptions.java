5ff2e5d19cc4d9f2e02b173ea85eb78f19e67e52
radimous <jurda92@gmail.com>
2026-02-05 04:16:47 +0100

    wallet module alignment

--- src/main/java/xyz/iwolfking/woldsvaults/init/ModOptions.java
+++ src/main/java/xyz/iwolfking/woldsvaults/init/ModOptions.java
@@ -10,7 +10,7 @@ import xyz.iwolfking.woldsvaults.client.invhud.LightmanWalletHudOptions;
 public class ModOptions {
     public static final VaultOption<InventoryHudElementOptions> GREEN_TRINKET = VaultOptionsRegistry.register("woldsOptions.json", WoldsVaults.id("inv_hud_trinket_green"), InventoryHudElementOptions.createDefault(new HudPosition(0.5F, 0.5F)), null, InventoryHudElementOptions.ADAPTER);
     public static final VaultOption<InventoryHudElementOptions> TRINKET_POUCH = VaultOptionsRegistry.register("woldsOptions.json", WoldsVaults.id("inv_hud_trinket_pouch"), InventoryHudElementOptions.createDefault(new HudPosition(0.5F, 0.5F)), null, InventoryHudElementOptions.ADAPTER);
-    public static final VaultOption<LightmanWalletHudOptions> LIGHTMAN_WALLET = VaultOptionsRegistry.register("woldsOptions.json", WoldsVaults.id("inv_hud_lightman_wallet"), LightmanWalletHudOptions.createDefault(new HudPosition(0.7F, 0.13F)), null, LightmanWalletHudOptions.ADAPTER);
+    public static final VaultOption<LightmanWalletHudOptions> LIGHTMAN_WALLET = VaultOptionsRegistry.register("woldsOptions.json", WoldsVaults.id("inv_hud_lightman_wallet"), LightmanWalletHudOptions.createDefault(new HudPosition(0.8F, 0.05F)), null, LightmanWalletHudOptions.ADAPTER);
 
     public static void init() {
         VaultOptionsRegistry.loadOptions();
