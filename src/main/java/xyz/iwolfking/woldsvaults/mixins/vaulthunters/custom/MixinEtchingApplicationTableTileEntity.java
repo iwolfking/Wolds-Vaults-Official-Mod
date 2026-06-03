0b11944ff3b7a036101e93b60830bac66602a7f1
iwolfking <thegreatiwk@gmail.com>
2026-06-03 14:09:58 -0500

    Allow etching sealed/unmodifiable gear

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinEtchingApplicationTableTileEntity.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/custom/MixinEtchingApplicationTableTileEntity.java
@@ -2,6 +2,7 @@ package xyz.iwolfking.woldsvaults.mixins.vaulthunters.custom;
 
 import iskallia.vault.block.entity.EtchingApplicationTableTileEntity;
 import iskallia.vault.gear.VaultGearRarity;
+import iskallia.vault.gear.data.VaultGearData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -13,8 +14,18 @@ public class MixinEtchingApplicationTableTileEntity {
         return false;
     }
 
+    @Redirect(method = "updateResult", at = @At(value = "INVOKE", target = "Liskallia/vault/gear/data/VaultGearData;isModifiable()Z"))
+    private boolean allowEtchingUnmodifiable(VaultGearData instance) {
+        return true;
+    }
+
     @Redirect(method = "getCompatibility", at = @At(value = "INVOKE", target = "Liskallia/vault/gear/VaultGearRarity;equals(Ljava/lang/Object;)Z"))
     private boolean allowEtchingUniquesCompat(VaultGearRarity instance, Object o) {
         return false;
+    }
+
+    @Redirect(method = "getCompatibility", at = @At(value = "INVOKE", target = "Liskallia/vault/gear/data/VaultGearData;isModifiable()Z"))
+    private boolean allowEtchingUnmodifiableCompat(VaultGearData instance) {
+        return true;
     }
 }
