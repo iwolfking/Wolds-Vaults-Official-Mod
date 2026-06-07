ed8b723a1537bf14de8a632b47c7a4e6b3065302
iwolfking <thegreatiwk@gmail.com>
2026-06-07 07:21:45 -0500

    Make Shockwave trap a bit less aggressive

--- src/main/java/xyz/iwolfking/woldsvaults/api/data/traps/WoldsChestTraps.java
+++ src/main/java/xyz/iwolfking/woldsvaults/api/data/traps/WoldsChestTraps.java
@@ -12,7 +12,7 @@ public class WoldsChestTraps {
     public static final List<MobBuffTrapEffect> MOB_BUFF_TRAP_EFFECTS = new ArrayList<>();
 
     static {
-        SHOCKWAVE_TRAP_EFFECTS.add(new ShockwaveTrapEffect("Shockwave", 6.0F, 3.0F));
+        SHOCKWAVE_TRAP_EFFECTS.add(new ShockwaveTrapEffect("Shockwave", 3.0F, 3.0F));
         MOB_BUFF_TRAP_EFFECTS.add(new MobBuffTrapEffect("Zoomies", 48.0D, 600, 2, List.of(MobEffects.MOVEMENT_SPEED)));
         MOB_BUFF_TRAP_EFFECTS.add(new MobBuffTrapEffect("Plating", 48.0D, 600, 2, List.of(MobEffects.DAMAGE_RESISTANCE, MobEffects.DAMAGE_BOOST)));
     }
