ea4e541a7af4e224ba383c21e5c7de8eaf69e426
iwolfking <thegreatiwk@gmail.com>
2026-06-25 11:53:25 -0500

    Remove unneeded class

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/MixinModConfigs.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/MixinModConfigs.java
@@ -21,12 +21,10 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xyz.iwolfking.woldsvaults.api.data.gear.UnusualModifiers;
 import xyz.iwolfking.woldsvaults.init.ModItems;
 import xyz.iwolfking.woldsvaults.mixins.vaulthunters.accessors.VaultGearCommonConfigAccessor;
 import xyz.iwolfking.woldsvaults.mixins.vaulthunters.accessors.VaultGearRollTypeConfigAccessor;
 import xyz.iwolfking.woldsvaults.mixins.vaulthunters.accessors.VaultGearRollTypeConfigRollTypeAccessor;
-import xyz.iwolfking.woldsvaults.mixins.vaulthunters.accessors.VaultGearTierConfigAccessor;
 
 import java.util.HashSet;
 import java.util.List;
@@ -49,20 +47,6 @@ public class MixinModConfigs {
     @Inject(method = "register", at = @At("TAIL"), remap = false)
     private static void onReloadConfigs(CallbackInfo ci) {
         xyz.iwolfking.woldsvaults.init.ModConfigs.register();
-
-        //Initialize unusual modifier values
-//        for(ResourceLocation config : ModConfigs.VAULT_GEAR_CONFIG.keySet()) {
-//            if(UnusualModifiers.UNUSUAL_MODIFIERS_MAP_PREFIX.containsKey(config)) {
-//                ((VaultGearTierConfigAccessor)ModConfigs.VAULT_GEAR_CONFIG.get(config)).getModifierGroup().put(VaultGearTierConfig.ModifierAffixTagGroup.valueOf("UNUSUAL_PREFIX"), UnusualModifiers.UNUSUAL_MODIFIERS_MAP_PREFIX.get(config));
-//            }
-//        }
-//
-//
-//        for(ResourceLocation config : ModConfigs.VAULT_GEAR_CONFIG.keySet()) {
-//            if(UnusualModifiers.UNUSUAL_MODIFIERS_MAP_SUFFIX.containsKey(config)) {
-//                ((VaultGearTierConfigAccessor)ModConfigs.VAULT_GEAR_CONFIG.get(config)).getModifierGroup().put(VaultGearTierConfig.ModifierAffixTagGroup.valueOf("UNUSUAL_SUFFIX"), UnusualModifiers.UNUSUAL_MODIFIERS_MAP_SUFFIX.get(config));
-//            }
-//        }
 
         Set<ResourceLocation> CURRENT_GEAR_CONFIGS =new HashSet<>(ModConfigs.VAULT_GEAR_CONFIG.keySet());
 
