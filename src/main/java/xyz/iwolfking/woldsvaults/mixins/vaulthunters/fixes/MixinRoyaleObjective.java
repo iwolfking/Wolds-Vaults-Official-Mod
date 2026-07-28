1499af5aa2c318c22997a8716eb10bbce4fdc662
iwolfking <thegreatiwk@gmail.com>
2026-07-28 09:37:18 -0500

    Timer pauses once boss spawns in Royale

--- src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/fixes/MixinRoyaleObjective.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixins/vaulthunters/fixes/MixinRoyaleObjective.java
@@ -3,14 +3,15 @@ package xyz.iwolfking.woldsvaults.mixins.vaulthunters.fixes;
 import com.llamalad7.mixinextras.sugar.Local;
 import iskallia.vault.core.event.common.ListenerLeaveEvent;
 import iskallia.vault.core.vault.Vault;
-import iskallia.vault.core.vault.objective.Objective;
-import iskallia.vault.core.vault.objective.RoyaleObjective;
 import iskallia.vault.core.vault.objective.VaultRoyaleObjective;
+import iskallia.vault.core.vault.time.TickClock;
+import iskallia.vault.core.world.storage.VirtualWorld;
 import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.iwolfking.woldsvaults.WoldsVaults;
 import xyz.iwolfking.woldsvaults.api.util.TrinketHelper;
 
 @Mixin(value = VaultRoyaleObjective.class, remap = false)
@@ -19,5 +20,17 @@ public class MixinRoyaleObjective {
     @Inject(method = "lambda$initServer$9", at = @At(value = "INVOKE", target = "Liskallia/vault/util/MiscUtils;clearPlayerInventory(Lnet/minecraft/world/entity/player/Player;)V", shift = At.Shift.BEFORE))
     private static void clearTrinketSlotsOnCompletion(Vault vault, ListenerLeaveEvent.Data data, CallbackInfo ci, @Local ServerPlayer player) {
             TrinketHelper.clearCurios(player);
+    }
+
+    @Inject(method = "tickServer", at = @At(value = "INVOKE", target = "Liskallia/vault/entity/boss/SkeletonGladiatorEntity;getHealth()F", shift = At.Shift.AFTER))
+    private void pauseTimer(VirtualWorld world, Vault vault, CallbackInfo ci) {
+        WoldsVaults.LOGGER.info("Ticking Boss pause");
+        if (!vault.get(Vault.CLOCK).has(TickClock.PAUSED)) {
+            vault.get(Vault.CLOCK).set(TickClock.PAUSED);
+        }
+
+        if (vault.get(Vault.CLOCK).has(TickClock.VISIBLE)) {
+            vault.get(Vault.CLOCK).remove(TickClock.VISIBLE);
+        }
     }
 }
