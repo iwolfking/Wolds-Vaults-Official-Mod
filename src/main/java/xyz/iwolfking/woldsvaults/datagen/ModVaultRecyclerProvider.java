dc0b1659b36c547582eacb2a853aa7de13067730
iwolfking <thegreatiwk@gmail.com>
2026-09-10 16:04:55 -0500

    Mod Boxes can be recycled into knowledge shards

--- src/main/java/xyz/iwolfking/woldsvaults/datagen/ModVaultRecyclerProvider.java
+++ src/main/java/xyz/iwolfking/woldsvaults/datagen/ModVaultRecyclerProvider.java
@@ -5,6 +5,7 @@ import iskallia.vault.init.ModItems;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Blocks;
 import xyz.iwolfking.vhapi.api.datagen.AbstractVaultRecyclerProvider;
 import xyz.iwolfking.vhapi.api.loaders.workstation.lib.GsonChanceItemStack;
 import xyz.iwolfking.woldsvaults.WoldsVaults;
@@ -18,6 +19,7 @@ public class ModVaultRecyclerProvider extends AbstractVaultRecyclerProvider {
     public void registerConfigs() {
        add("new_recycleables", builder -> {
            builder.add(VaultMod.id("deck_socket"), chanceStack(ModItems.SILVER_SCRAP, 0, 10, 1.0F), chanceStack(ModItems.BLACK_OPAL_GEM, 0, 4, 1.0F), chanceStack(ModItems.CARD_JUICE, 0, 6, 1.0F));
+           builder.add(VaultMod.id("mod_box"), chanceStack(ModItems.KNOWLEDGE_STAR_SHARD, 1, 4, 1.0F), chanceStack(Blocks.AIR, 0, 0, 0.0F), chanceStack(Blocks.AIR, 0, 0, 0.0F));
        });
     }
 
