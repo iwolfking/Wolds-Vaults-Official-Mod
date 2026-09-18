20d398c3c8edb000277858fdf39b1986c262ef08
iwolfking <thegreatiwk@gmail.com>
2026-09-18 07:32:52 -0500

    Card deck recycling

--- src/main/java/xyz/iwolfking/woldsvaults/datagen/ModVaultRecyclerProvider.java
+++ src/main/java/xyz/iwolfking/woldsvaults/datagen/ModVaultRecyclerProvider.java
@@ -18,8 +18,9 @@ public class ModVaultRecyclerProvider extends AbstractVaultRecyclerProvider {
     @Override
     public void registerConfigs() {
        add("new_recycleables", builder -> {
-           builder.add(VaultMod.id("deck_socket"), chanceStack(ModItems.SILVER_SCRAP, 0, 10, 1.0F), chanceStack(ModItems.BLACK_OPAL_GEM, 0, 4, 1.0F), chanceStack(ModItems.CARD_JUICE, 0, 6, 1.0F));
-           builder.add(VaultMod.id("mod_box"), chanceStack(ModItems.KNOWLEDGE_STAR_SHARD, 1, 4, 1.0F), chanceStack(Blocks.AIR, 0, 0, 0.0F), chanceStack(Blocks.AIR, 0, 0, 0.0F));
+           builder.add(ModItems.DECK_SOCKET.getRegistryName(), chanceStack(ModItems.SILVER_SCRAP, 0, 10, 1.0F), chanceStack(ModItems.BLACK_OPAL_GEM, 0, 4, 1.0F), chanceStack(ModItems.CARD_JUICE, 0, 6, 1.0F));
+           builder.add(ModItems.MOD_BOX.getRegistryName(), chanceStack(ModItems.KNOWLEDGE_STAR_SHARD, 1, 4, 1.0F), chanceStack(Blocks.AIR, 0, 0, 0.0F), chanceStack(Blocks.AIR, 0, 0, 0.0F));
+           builder.add(ModItems.CARD_DECK.getRegistryName(), chanceStack(ModItems.POG, 1, 1, 1.0F), chanceStack(ModItems.BLACK_OPAL_GEM, 4, 16, 1.0F), chanceStack(Blocks.AIR, 0, 0, 0.0F));
        });
     }
 
